f2926c96dff9ad09c7640fba090420ced54a7593
JHon <jhonmendoza2530@gmail.com>
2023-05-02 22:35:36 -0500

    first commit

---
+++ ProyectoTest/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+
+        public int IdCliente { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+
+        public string Clave { get; set; }
+        public string ConfirmarContrasena { get; set; }
+        public bool EsAdministrador { get; set; }
+        public bool Reestablecer { get; set; }
+        public bool Activo { get; set; }
+    }
+}
